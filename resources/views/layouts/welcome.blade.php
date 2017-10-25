79d2f48e27024b53b877fd2f0a0b4c8a59e89b1c
Evert Trollip <trollip.evert@gmail.com>
2017-10-25 07:47:06 +0200

    Adds the moved layout views again.

---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Online CV
+                </div>
+              <div class="links">
+                    <a href="/login">Log In</a>
+                    <a data-toggle="modal" data-target="#registermodal" id="gotoregister" href="#">Register</a>
+                    {{--  <a id="register" href="/register">Register</a>  --}}
+
+                </div>
+               <div id="registermodal" class="modal fade" role="dialog" style="margin-top:20%">
+                    <div class="modal-dialog">
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Please select the user type you want to register as:</h4>
+                        </div>
+                        <div class="modal-body">
+                           <a href="/register/student"><button class="btn btn-primary" style="width:150px;">Student</button></a>
+                            <a href="/register/endorser"></a><button class="btn btn-warning" style="width:150px;">Endorser</button></a>
+                           <a href="/register/hub"></a><button class="btn btn-info" style="width:150px;">Hub</button></a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!-- Scripts -->
+<script src="adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Bootbox 3.3.7 -->
+<script src="js/bootbox.min.js"></script>
+    </body>
+</html>
